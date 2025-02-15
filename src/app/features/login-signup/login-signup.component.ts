3041571a994e8ffc9049b3ca9e0ee20e0ebc0fb3
Saran-Sandeep <115928655+Saran-Sandeep@users.noreply.github.com>
2025-02-15 20:04:09 +0530

    Added signup content

--- src/app/features/login-signup/login-signup.component.ts
+++ src/app/features/login-signup/login-signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, signal, SimpleChanges } from '@angular/core';
 import {
   FormControl,
   Validators,
@@ -10,6 +10,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { PasswordValidator } from './password.validator';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login-signup',
   imports: [
@@ -17,11 +18,14 @@ import { AuthService } from '../../shared/services/auth.service';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.css',
 })
 export class LoginSignupComponent {
+  isLogin = signal(true);
+
   constructor(private router: Router, private authService: AuthService) {}
 
   emailFormControl = new FormControl('', [
@@ -38,21 +42,36 @@ export class LoginSignupComponent {
     if (this.emailFormControl.valid && this.passwordFormControl.valid) {
       const email = this.emailFormControl.value;
       const password = this.passwordFormControl.value;
-      // if (email === 'admin@gmail.com' && password === 'admin123') {
-      //   this.router.navigateByUrl('landing-page');
-      // }
 
       if (email && password) {
         this.authService.login(email, password);
         if (this.authService.isAuthenticated()) {
           this.router.navigateByUrl('landing-page');
         } else {
-          alert('incorrect email or password');
+          alert('Incorrect email or password');
         }
       }
     } else {
-      // console.log('Form is invalid');
-      alert('login unsuccesful');
+      alert('Login unsuccesful');
     }
   }
+
+  createAccount(): void {
+    this.isLogin.set(false);
+    console.log(this.isLogin());
+  }
+
+  signupNameFormControl = new FormControl('', [Validators.required]);
+
+  signupEmailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  signupPasswordFormControl = new FormControl('', [
+    Validators.required,
+    // PasswordValidator.strongPassword
+  ]);
+
+  onSignupSubmit(): void {}
 }
